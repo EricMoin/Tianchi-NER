1ff0d8b6520faab2e1bb62b87893fd0edf05ebae
EricMoin <wu_ming12@foxmail.com>
2025-05-24 19:19:34 +0800

    process

--- process.py
+++ process.py
@@ -1,11 +1,11 @@
 import json
 
 data_json = []
-with open("data/dev.json", "r", encoding="utf-8") as file:
-    for line in file:
-        data_json.append(json.loads(line))
-
-with open('data/extra_dev_2.txt', "w", encoding="utf-8") as file:
-    for data in data_json:
-        file.write(data['sentence1'] + "\n")
-        file.write(data['sentence2'] + "\n")
+with open("data/address.txt", "r", encoding="utf-8") as file:
+    total_len = len(file)
+    for i in range(30000):
+        if i % 1000 == 0:
+            print(f"Processing line {i} of {total_len}")
+        line = file.readline()
+        if line:
+            data_json.append(line)
